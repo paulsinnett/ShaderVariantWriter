7a467d66ff7649c8a4ab0b63460813f7a21559f7
Paul Sinnett <paul.sinnett@gmail.com>
2019-12-05 00:23:54 +0000

    Initial commit

---
+++ Assets/ShaderVariantWriter/Editor/ShaderVariantWriter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu()]
+public class ShaderVariantWriter : ScriptableObject
+{
+	public SceneAsset scene;
+	public List<Shader> additionalShaders;
+	public List<GameObject> additionalPrefabs;
+	public List<Material> additionalMaterials;
+}
